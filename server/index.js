c1119aab273c74f6828c5a741ce6d65e5fc81e09
Ryan Pearce <n01366544@humber.ca>
2025-03-08 15:42:15 -0500

    Test (Ensuring ignore works as it is not graying out.)

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use("/", (req, res) => {
+app.use("/", (req, res) => { 
     res.send("Howdy!");
 })
 
