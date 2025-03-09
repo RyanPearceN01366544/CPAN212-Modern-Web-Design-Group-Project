2b1b5a57dec43bde22da2b3cb72a5a386b3dbb82
Ibby-18 <n01623109@humber.ca>
2025-03-08 19:30:58 -0500

    created client template

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,21 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+>>>>>>> 6e765cf (created client template)
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
+<<<<<<< HEAD
   </React.StrictMode>,
 =======
 import { StrictMode } from 'react'
@@ -21,6 +28,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 >>>>>>> 01b19ea (created client template)
+=======
+  </StrictMode>,
+>>>>>>> 6e765cf (created client template)
 )
 =======
 import React from 'react';
