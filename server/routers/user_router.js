99a09da630fce8ba530599987d75af51d126411e
LuckyNinja1 <n01366544@humber.ca>
2025-04-06 00:04:16 -0400

    What the heck... Will have to test everything tomorrow but fixing Cart.

--- server/routers/user_router.js
+++ server/routers/user_router.js
@@ -881,17 +881,46 @@ user_router.get("/Cart", auth.verifyToken, async(req, res) => {
     console.log(err_);
     res.status(400).json({message: "An Unexpected Error has Occurred!"});
   }
+<<<<<<< HEAD
 });
+=======
+})
+>>>>>>> 9ac50d4 (What the heck... Will have to test everything tomorrow but fixing Cart.)
 user_router.post("/Cart", auth.verifyToken, async(req, res) => {
   try{
     const {product, quantity} = req.body;
     const user = await User.findById(req.user.userID);
+<<<<<<< HEAD
     
   }
   catch (err_){
     console.log(err_);
 =======
 >>>>>>> f4358d8 (Fixed the Cart)
+    return res.status(400).json({message: "An Unexpected Error Has Occured!"});
+  }
+});
+user_router.delete("/Cart", auth.verifyToken, async(req, res) => {
+  try{
+    const {product, quantity} = req.body;
+    const user = await User.findById(req.user.userID);
+=======
+>>>>>>> 9ac50d4 (What the heck... Will have to test everything tomorrow but fixing Cart.)
+  
+    for (let x_ = 0; x_ < user.cart.length; x++) { // R: Loop through the cart.
+      if (user.cart[x_].product === product){ // R: if the product is the same key we're looking for...
+        user.cart[x_].quantity -= quantity; // R: Decrease by quantity.
+        if (user.cart[x_].quantity <= 0) { // R: If the quantity is too low...
+          user.cart.splice(x_, 1); // R: Remove it from the array.
+        }
+        break; // R: Stop the loop.
+      }
+    }
+    await user.save(); // R: Save changes.
+    return res.json(user.cart); // R: Return the cart.
+  }
+  catch (err_){
+    console.log(err_);
     return res.status(400).json({message: "An Unexpected Error Has Occured!"});
   }
 });
